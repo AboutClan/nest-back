c9545e378aeb5bfff0303964d089406704d65d53
beming-dev <mingfordev@gmail.com>
2024-09-23 11:25:58 +0900

    Add chat module

---
+++ src/chatz/statics/chat.module.ts
@@ -0,0 +1,11 @@
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { ChatContoller } from '../chat.controller';
+import { ChatService } from '../chat.service';
+
+@Module({
+  imports: [],
+  controllers: [ChatContoller],
+  providers: [ChatService],
+  exports: [ChatService],
+})
+export class AppModule {}
