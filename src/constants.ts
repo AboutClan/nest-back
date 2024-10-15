2bd2d61910de94700805b61177a59dbfa1f28e45
beming-dev <mingfordev@gmail.com>
2024-10-15 11:38:22 +0900

    Update alphabet

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const C_simpleUser =
   '_id name profileImage uid score avatar comment location';
 
-export const ALPHABET_COLLECTION = ['A', 'B', 'O', 'U', 'T'];
+export const ALPHABET_COLLECTION = ['A', 'b', 'o', 'u', 'T'];
