fc08f23e4361c321d9efdceaf1b4966efbbd7d9f
beming-dev <mingfordev@gmail.com>
2024-10-31 01:16:17 +0900

    Update cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
-    allowedHeaders: 'Content-Type,Authorization', // 허용할 헤더
-  });
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
