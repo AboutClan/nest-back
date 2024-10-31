8aad034b071aaed5c9bd06f529f20bd91b2483cd
beming-dev <mingfordev@gmail.com>
2024-10-31 13:17:55 +0900

    Add counter repository

---
+++ src/counter/counter.repository.ts
@@ -0,0 +1,8 @@
+import { ICounter } from './entity/counter.entity';
+
+export interface CounterRepository {
+  findByKeyAndLoc(key: string, location: string): Promise<ICounter>;
+  increaseByKeyAndLoc(key: string, location: string): Promise<any>;
+  createCounter(counterData): Promise<ICounter>;
+  increaseByKey(key: string): Promise<any>;
+}
