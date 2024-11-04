fa15a5d5c22317d0b7dd05dee3fed371689a1ff3
beming-dev <mingfordev@gmail.com>
2024-11-04 10:26:50 +0900

    Add test log

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('test');
   const app = await NestFactory.create(AppModule, { bodyParser: true });
   app.enableCors({
     origin: 'https://studyabout.herokuapp.com',
