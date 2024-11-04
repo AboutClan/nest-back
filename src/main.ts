a16a368d668855344652a0a29aa9ca197f0daf6d
beming-dev <mingfordev@gmail.com>
2024-11-04 10:44:46 +0900

    Update port binding

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('test');
   const app = await NestFactory.create(AppModule, { bodyParser: true });
   app.enableCors({
     origin: 'https://studyabout.herokuapp.com',
@@ -10,7 +9,7 @@ async function bootstrap() {
     credentials: true,
     allowedHeaders: '*',
   });
-  console.log('hello');
-  await app.listen(3001);
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
 }
 bootstrap();
