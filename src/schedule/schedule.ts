f7ba4a4923996d5f3a27c2a074e7a366cc0dbd49
beming-dev <mingfordev@gmail.com>
2024-11-06 00:15:37 +0900

    Test log

--- src/schedule/schedule.ts
+++ src/schedule/schedule.ts
@@ -23,56 +23,63 @@ export class NotificationScheduler {
     private readonly adminVoteService: AdminVoteService,
     @InjectModel('User') private readonly User: Model<IUser>,
   ) {}
-
   @Cron('0 18 * * 2,3,5,6', {
     timeZone: 'Asia/Seoul',
   })
   async sendNotification() {
-    console.log(1);
+    try {
+      await this.webPushService.sendNotificationAllUser();
+      await this.fcmService.sendNotificationAllUser(
+        '스터디 투표',
+        '스터디 마감이 얼마 남지 않았어요. 지금 신청하세요!',
+      );
+      this.logger.log('Notifications sent successfully to all users.');
+    } catch (error) {
+      this.logger.error('Error sending notifications:', error);
+      throw new Error(error);
+    }
   }
-  // @Cron('0 18 * * 2,3,5,6', {
-  //   timeZone: 'Asia/Seoul',
-  // })
-  // async sendNotification() {
-  //   try {
-  //     await this.webPushService.sendNotificationAllUser();
-  //     await this.fcmService.sendNotificationAllUser(
-  //       '스터디 투표',
-  //       '스터디 마감이 얼마 남지 않았어요. 지금 신청하세요!',
-  //     );
-  //     this.logger.log('Notifications sent successfully to all users.');
-  //   } catch (error) {
-  //     this.logger.error('Error sending notifications:', error);
-  //     throw new Error(error);
-  //   }
-  // }
 
-  // @Cron(CronExpression.EVERY_DAY_AT_9AM, {
-  //   timeZone: 'Asia/Seoul',
-  // })
-  // async announceVoteResult() {
-  //   try {
-  //     const date = dayjs().format('YYYY-MM-DD');
-  //     await this.adminVoteService.confirm(date);
-  //     await this.webPushService.sendNotificationVoteResult();
-  //     await this.fcmService.sendNotificationVoteResult();
-  //     this.logger.log('Vote result notifications sent successfully.');
-  //   } catch (error) {
-  //     this.logger.error('Error sending vote result notifications:', error);
-  //     throw new Error(error);
-  //   }
-  // }
+  @Cron(CronExpression.EVERY_DAY_AT_9AM, {
+    timeZone: 'Asia/Seoul',
+  })
+  async announceVoteResult() {
+    console.log('1');
+    try {
+      const date = dayjs().format('YYYY-MM-DD');
+      await this.adminVoteService.confirm(date);
+      await this.webPushService.sendNotificationVoteResult();
+      await this.fcmService.sendNotificationVoteResult();
+      this.logger.log('Vote result notifications sent successfully.');
+    } catch (error) {
+      this.logger.error('Error sending vote result notifications:', error);
+      throw new Error(error);
+    }
+  }
 
-  // @Cron('0 0 1 * *', {
-  //   timeZone: 'Asia/Seoul',
-  // })
-  // async resetMonthlyScores() {
-  //   try {
-  //     await this.User.updateMany({}, { monthScore: 0 });
-  //     this.logger.log('Monthly scores reset successfully.');
-  //   } catch (error) {
-  //     this.logger.error('Error resetting monthly scores:', error);
-  //     throw new Error(error);
-  //   }
-  // }
+  @Cron('0 0 1 * *', {
+    timeZone: 'Asia/Seoul',
+  })
+  async resetMonthlyScores() {
+    try {
+      await this.User.updateMany({}, { monthScore: 0 });
+      this.logger.log('Monthly scores reset successfully.');
+    } catch (error) {
+      this.logger.error('Error resetting monthly scores:', error);
+      throw new Error(error);
+    }
+  }
+
+  @Cron('0 0 0 * * 1', {
+    timeZone: 'Asia/Seoul',
+  })
+  async initTargetHour() {
+    try {
+      await this.User.updateMany({}, { weekStudyTragetHour: 0 });
+      await this.User.updateMany({}, { weekStudyAccumulationMinutes: 0 });
+      console.log('target hour init success');
+    } catch (err: any) {
+      throw new Error(err);
+    }
+  }
 }
