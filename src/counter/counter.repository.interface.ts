3a8ea9088abd88aa75ddeb6fdd210f5e09a9b613
beming-dev <mingfordev@gmail.com>
2024-11-07 14:14:01 +0900

    Update counter to use repository

--- src/counter/counter.repository.interface.ts
+++ src/counter/counter.repository.interface.ts
@@ -4,5 +4,5 @@ export interface CounterRepository {
   findByKeyAndLoc(key: string, location: string): Promise<ICounter>;
   increaseByKeyAndLoc(key: string, location: string): Promise<any>;
   createCounter(counterData): Promise<ICounter>;
-  increaseByKey(key: string): Promise<any>;
+  increaseByKey(key: string): Promise<ICounter>;
 }
--- src/counter/couter.module.ts
+++ src/counter/couter.module.ts
@@ -2,11 +2,17 @@ import { ClassProvider, Module } from '@nestjs/common';
 import { CounterSchema } from 'src/counter/entity/counter.entity';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CounterService } from './counter.service';
-import { ICOUNTER_SERVICE } from 'src/utils/di.tokens';
+import { ICOUNTER_REPOSITORY, ICOUNTER_SERVICE } from 'src/utils/di.tokens';
+import { MongoCounterRepository } from './counter.repository';
 
 const counterServiceProvider: ClassProvider = {
   provide: ICOUNTER_SERVICE,
   useClass: CounterService,
+};
+
+const counterRepositoryProvider: ClassProvider = {
+  provide: ICOUNTER_REPOSITORY,
+  useClass: MongoCounterRepository,
 };
 
 @Module({
@@ -14,7 +20,7 @@ const counterServiceProvider: ClassProvider = {
     MongooseModule.forFeature([{ name: 'Counter', schema: CounterSchema }]),
   ],
   controllers: [],
-  providers: [counterServiceProvider],
-  exports: [counterServiceProvider, MongooseModule],
+  providers: [counterServiceProvider, counterRepositoryProvider],
+  exports: [counterServiceProvider, MongooseModule, counterRepositoryProvider],
 })
 export class CounterModule {}
