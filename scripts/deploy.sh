94d886c8f493f96a18a5e1a0a282a85f6e554989
beming-dev <mingfordev@gmail.com>
2025-01-01 23:53:12 +0900

    Update port

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,4 +3,4 @@ cd /home/ec2-user/app
 docker pull 835080864477.dkr.ecr.ap-northeast-2.amazonaws.com/beming/about
 docker stop nest-app || true
 docker rm nest-app || true
-docker run -d --name my-app -p 80:3000 835080864477.dkr.ecr.ap-northeast-2.amazonaws.com/beming/about
+docker run -d --name my-app -p 80:3001 835080864477.dkr.ecr.ap-northeast-2.amazonaws.com/beming/about
