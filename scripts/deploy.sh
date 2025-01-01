2b5f7ad40c28e02ed74d27c699387ca25eb4e32f
beming-dev <mingfordev@gmail.com>
2025-01-02 00:32:36 +0900

    Add latest

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ec2-user/app
-docker pull 835080864477.dkr.ecr.ap-northeast-2.amazonaws.com/beming/about
+docker pull 835080864477.dkr.ecr.ap-northeast-2.amazonaws.com/beming/about:latest
 docker stop nest-app || true
 docker rm nest-app || true
-docker run -d --name my-app -p 80:3001 835080864477.dkr.ecr.ap-northeast-2.amazonaws.com/beming/about
+docker run -d --name my-app -p 80:3001 835080864477.dkr.ecr.ap-northeast-2.amazonaws.com/beming/about:latest
