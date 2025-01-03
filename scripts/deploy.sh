1236861b39c8c5b009744c1b245b62ae972c8006
beming-dev <mingfordev@gmail.com>
2025-01-03 10:24:21 +0900

    Update user

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/app
+cd /home/ubuntu/app
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 835080864477.dkr.ecr.ap-northeast-2.amazonaws.com/beming/about
 docker pull 835080864477.dkr.ecr.ap-northeast-2.amazonaws.com/beming/about:latest
 docker stop nest-app || true
