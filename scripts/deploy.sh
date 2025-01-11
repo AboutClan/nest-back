cbcf7a0e5f6aab0b1e961757fc6afdee5cc92580
beming-dev <mingfordev@gmail.com>
2025-01-11 15:52:24 +0900

    Update secret

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,18 +2,22 @@
 cd /home/ubuntu/app
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 294951093594.dkr.ecr.ap-northeast-2.amazonaws.com/about
 
-MONGODB_URI=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.MONGODB_URI')
-cryptoKey=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.cryptoKey')
-PAPERTRAIL_API_TOKEN=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.cryptoKey')
-AWS_KEY=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.AWS_KEY')
-AWS_ACCESS_KEY=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.AWS_ACCESS_KEY')
-PUBLIC_KEY=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.PUBLIC_KEY')
-PRIVATE_KEY=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.PRIVATE_KEY')
-NEW_RELIC_APP_NAME=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.NEW_RELIC_APP_NAME')
-NEW_RELIC_LICENSE_KEY=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.NEW_RELIC_LICENSE_KEY')
-NEW_RELIC_LOG=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.NEW_RELIC_LOG')
-NEW_RELIC_NO_CONFIG_FILE=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.NEW_RELIC_NO_CONFIG_FILE')
-NEW_RELIC_ENABLED=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text | jq -r '.NEW_RELIC_ENABLED')
+# Secrets Manager에서 한 번만 시크릿 가져오기
+SECRETS=$(aws secretsmanager get-secret-value --secret-id about/backend --query SecretString --output text)
+
+# 필요한 시크릿 환경 변수 설정
+MONGODB_URI=$(echo $SECRETS | jq -r '.MONGODB_URI')
+cryptoKey=$(echo $SECRETS | jq -r '.cryptoKey')
+PAPERTRAIL_API_TOKEN=$(echo $SECRETS | jq -r '.PAPERTRAIL_API_TOKEN')
+AWS_KEY=$(echo $SECRETS | jq -r '.AWS_KEY')
+AWS_ACCESS_KEY=$(echo $SECRETS | jq -r '.AWS_ACCESS_KEY')
+PUBLIC_KEY=$(echo $SECRETS | jq -r '.PUBLIC_KEY')
+PRIVATE_KEY=$(echo $SECRETS | jq -r '.PRIVATE_KEY')
+NEW_RELIC_APP_NAME=$(echo $SECRETS | jq -r '.NEW_RELIC_APP_NAME')
+NEW_RELIC_LICENSE_KEY=$(echo $SECRETS | jq -r '.NEW_RELIC_LICENSE_KEY')
+NEW_RELIC_LOG=$(echo $SECRETS | jq -r '.NEW_RELIC_LOG')
+NEW_RELIC_NO_CONFIG_FILE=$(echo $SECRETS | jq -r '.NEW_RELIC_NO_CONFIG_FILE')
+NEW_RELIC_ENABLED=$(echo $SECRETS | jq -r '.NEW_RELIC_ENABLED')
 
 docker pull 294951093594.dkr.ecr.ap-northeast-2.amazonaws.com/about:latest
 docker stop nest-app || true
