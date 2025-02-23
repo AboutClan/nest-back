9a58e400a68ebf38937af76152d4f7485bbe463d
beming-dev <mingfordev@gmail.com>
2025-02-23 23:32:09 +0900

    Update error logging

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -3,18 +3,22 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  Inject,
+  LoggerService,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  constructor(@Inject('winston') private readonly logger: LoggerService) {}
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status = exception.getStatus();
 
-    console.error(exception);
+    this.logger.error(exception);
 
     response.status(status).json({
       statusCode: status,
