2666b9e77296281d254a8b8e772d18752c626471
beming-dev <mingfordev@gmail.com>
2025-03-10 20:17:50 +0900

    Add absence api

--- src/vote2/vote2.controller.ts
+++ src/vote2/vote2.controller.ts
@@ -4,7 +4,9 @@ import {
   Delete,
   Get,
   Inject,
+  Patch,
   Post,
+  Query,
   Req,
 } from '@nestjs/common';
 import {
@@ -25,6 +27,15 @@ export class Vote2Controller {
     await this.voteService2.setResult(new Date());
   }
 
+  @Get('arrived')
+  async getArrived(
+    @Query('startDay') startDay: string,
+    @Query('endDay') endDay: string,
+  ) {
+    const results = await this.voteService2.getArrivedPeriod(startDay, endDay);
+    return results;
+  }
+
   @Delete(':date')
   async deleteVote(@Req() req: Request): Promise<any> {
     const { date } = req;
@@ -36,6 +47,24 @@ export class Vote2Controller {
 
   @Post(':date')
   async setVote(
+    @Req() req: Request,
+    @Body() createVoteDTO: CreateNewVoteDTO,
+  ): Promise<any> {
+    const { latitude, longitude, start, end } = createVoteDTO;
+    const { date } = req;
+
+    await this.voteService2.setVote(date, {
+      latitude,
+      longitude,
+      start,
+      end,
+    });
+
+    return 'success';
+  }
+
+  @Patch(':date')
+  async patchVote(
     @Req() req: Request,
     @Body() createVoteDTO: CreateNewVoteDTO,
   ): Promise<any> {
@@ -87,5 +116,25 @@ export class Vote2Controller {
     await this.voteService2.setArrive(date, memo);
 
     return 'success';
+  }
+
+  @Get(':date/absence')
+  async segetAbsence(@Req() req: Request): Promise<any> {
+    const { date } = req; // 미들웨어에서 설정된 date 값 가져오기
+
+    const result = await this.voteService2.getAbsence(date);
+    return result;
+  }
+
+  @Post(':date/absence')
+  async setAbsence(
+    @Req() req: Request,
+    @Body() body: { message: string },
+  ): Promise<any> {
+    const { message = '' } = body; // message 값, 기본값 설정
+    const { date } = req; // 미들웨어에서 설정된 date 값 가져오기
+
+    const result = await this.voteService2.setAbsence(date, message);
+    return result;
   }
 }
