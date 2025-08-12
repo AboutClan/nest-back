428a31263bbacf27f951f085af29a75d5a75533b
beming-dev <mingfordev@gmail.com>
2025-08-12 20:49:21 +0900

    Update user controller to use dto

--- src/routes/user/dto.ts
+++ src/routes/user/dto.ts
@@ -63,3 +63,141 @@ export class PatchBelongDto {
   @IsString()
   belong: string;
 }
+
+export class PatchIsPrivateDto {
+  @IsNotEmpty()
+  isPrivate: boolean;
+}
+
+export class SetMonthStudyTargetDto {
+  @IsNotEmpty()
+  @IsNumber()
+  monthStudyTarget: number;
+}
+
+export class PatchIsLocationSharingDeniedDto {
+  @IsNotEmpty()
+  isLocationSharingDenided: boolean;
+}
+
+export class PatchRestDto {
+  @IsNotEmpty()
+  info: any;
+}
+
+export class ParticipationRateQueryDto {
+  @IsNotEmpty()
+  @IsString()
+  startDay: string;
+
+  @IsNotEmpty()
+  @IsString()
+  endDay: string;
+
+  @IsOptional()
+  @IsString()
+  location?: string | null;
+
+  @IsOptional()
+  summary: boolean;
+}
+
+export class VoteRateQueryDto {
+  @IsNotEmpty()
+  @IsString()
+  startDay: string;
+
+  @IsNotEmpty()
+  @IsString()
+  endDay: string;
+}
+
+export class UpdateProfileDto {
+  @IsNotEmpty()
+  registerForm: any;
+}
+
+export class UpdateDepositDto {
+  @IsOptional()
+  @IsNumber()
+  deposit: number;
+
+  @IsOptional()
+  @IsString()
+  message: string;
+
+  @IsOptional()
+  sub: any;
+}
+
+export class UpdateScoreDto {
+  @IsOptional()
+  @IsNumber()
+  score: number;
+
+  @IsOptional()
+  @IsString()
+  message: string;
+
+  @IsOptional()
+  sub: any;
+}
+
+export class UpdatePointDto {
+  @IsOptional()
+  @IsNumber()
+  point: number;
+
+  @IsOptional()
+  @IsString()
+  message: string;
+
+  @IsOptional()
+  sub: any;
+}
+
+export class PatchStudyTargetHourDto {
+  @IsNotEmpty()
+  @IsNumber()
+  hour: number;
+}
+
+export class PatchLocationDetailDto {
+  @IsOptional()
+  @IsString()
+  text: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lat: string;
+
+  @IsNotEmpty()
+  @IsString()
+  lon: string;
+}
+
+export class AddBadgeDto {
+  @IsNotEmpty()
+  @IsString()
+  userId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  badgeName: string;
+}
+
+export class SelectBadgeDto {
+  @IsNotEmpty()
+  @IsNumber()
+  badgeIdx: number;
+}
+
+export class UpdateTicketDto {
+  @IsNotEmpty()
+  @IsNumber()
+  ticketNum: number;
+
+  @IsNotEmpty()
+  @IsString()
+  type: 'gather' | 'groupStudy' | 'groupOnline' | 'groupOffline';
+}
