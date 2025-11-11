16f3275eaf2439c77b4f3e2ef164953ccdbae201
beming-dev <mingfordev@gmail.com>
2025-11-11 19:37:10 +0900

    Update prize for other criteria

--- src/routes/prize/prize.service.ts
+++ src/routes/prize/prize.service.ts
@@ -4,6 +4,7 @@ import { Inject } from '@nestjs/common';
 import { IPRIZE_REPOSITORY, IUSER_REPOSITORY } from 'src/utils/di.tokens';
 import { ENTITY } from 'src/Constants/ENTITY';
 import { IUserRepository } from '../user/UserRepository.interface';
+import { UserService } from '../user/user.service';
 
 export class PrizeService {
   prizeList = PRIZE;
@@ -12,6 +13,7 @@ export class PrizeService {
     private readonly PrizeRepository: IPrizeRepository,
     @Inject(IUSER_REPOSITORY)
     private readonly UserRepository: IUserRepository,
+    private readonly userService: UserService,
   ) {
     this.prizeList = PRIZE;
   }
@@ -56,28 +58,28 @@ export class PrizeService {
       this.recordMonthPrize(rank, top5UserIds);
 
       if (rank === ENTITY.USER.RANK_SILVER) {
-        const pointList = [5000, 3000, 2000, 1000, 100];
+        const pointList = [3000, 2000, 1000, 1000, 100];
         for (let i = 0; i < top5UserIds.length; i++) {
           const userId = top5UserIds[i];
           const point = pointList[i] || 1000; // 기본값 1000
-          // await this.updatePointById(
-          //   point,
-          //   `월간 ${rank} 등수 보상`,
-          //   '월간 점수 보상',
-          //   userId,
-          // );
+          await this.userService.updatePointById(
+            point,
+            `월간 ${rank} 등수 보상`,
+            '월간 점수 보상',
+            userId,
+          );
         }
       } else if (rank === ENTITY.USER.RANK_BRONZE) {
         const pointList = [3000, 2000, 1000, 1000, 1000];
         for (let i = 0; i < top5UserIds.length; i++) {
           const userId = top5UserIds[i];
           const point = pointList[i] || 1000; // 기본값 1000
-          // await this.updatePointById(
-          //   point,
-          //   `월간 ${rank} 등수 보상`,
-          //   '월간 점수 보상',
-          //   userId,
-          // );
+          await this.userService.updatePointById(
+            point,
+            `월간 ${rank} 등수 보상`,
+            '월간 점수 보상',
+            userId,
+          );
         }
       }
     }
@@ -109,10 +111,28 @@ export class PrizeService {
       user._id.toString(),
     );
 
-    this.recordMonthPrize(
-      ENTITY.USER.RANK_TEMPERATURE,
-      top5TemperatureUserIds as string[],
-    );
-    this.recordMonthPrize(ENTITY.USER.RANK_STUDY, top5StudyUserIds as string[]);
+    const pointList = [5000, 3000, 1000, 1000, 1000];
+    for (let i = 0; i < top5TemperatureUserIds.length; i++) {
+      const userId = top5TemperatureUserIds[i];
+      const point = pointList[i] || 1000; // 기본값 1000
+      await this.userService.updatePointById(
+        point,
+        `월간 ${ENTITY.USER.RANK_TEMPERATURE} 등수 보상`,
+        '월간 점수 보상',
+        userId,
+      );
+    }
+    for (let i = 0; i < top5StudyUserIds.length; i++) {
+      const userId = top5StudyUserIds[i];
+      const point = pointList[i] || 1000; // 기본값 1000
+      await this.userService.updatePointById(
+        point,
+        `월간 ${ENTITY.USER.RANK_STUDY} 등수 보상`,
+        '월간 점수 보상',
+        userId,
+      );
+    }
+    this.recordMonthPrize(ENTITY.USER.RANK_TEMPERATURE, top5TemperatureUserIds);
+    this.recordMonthPrize(ENTITY.USER.RANK_STUDY, top5StudyUserIds);
   }
 }
